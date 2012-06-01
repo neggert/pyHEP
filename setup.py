292be595ce4fdad837d8924112d28113b2f0fb4e
Nic Eggert <nse23@cornell.edu>
2012-06-01 15:22:59 -0400

    Re-organized to allow distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyHEP',
+      version='0.1',
+      description='Python for High Energy Physics',
+      author='Nic Eggert',
+      author_email='nse23@cornell.edu',
+      # url='http://www.python.org/sigs/distutils-sig/',
+      packages=['pyhep'],
+     )
